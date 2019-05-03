41dc97e787b1d5263da0bcc7893d263eadd31ff5
Palkovsky <dawidmacek42@gmail.com>
2019-05-03 22:18:37 +0200

    Update Bitwise.scala
    error in bitwise manipulation

--- src/main/scala/bitwise/Bitwise.scala
+++ src/main/scala/bitwise/Bitwise.scala
@@ -6,7 +6,7 @@ object Bitwise {
 
   def word(lower: UByte, upper: UByte): UShort = (UShort(upper.toInt) << 8) + UShort(lower.toInt)
 
-  def upper(word: UShort): UByte = UByte((word & UShort(0xFF00)).toInt)
+  def upper(word: UShort): UByte = UByte(((word & UShort(0xFF00)) >> 8).toInt)
 
   def lower(word: UShort): UByte = UByte((word & UShort(0x00FF)).toInt)
 }
